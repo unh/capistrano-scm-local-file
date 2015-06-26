e39f3f4ec36ae2e4491c1f5d87fa4a57e3ba5bc1
unh <formsgs@gmail.com>
2015-06-26 11:15:22 +0900

    try add hidden files to release

--- lib/capistrano/local_file.rb
+++ lib/capistrano/local_file.rb
@@ -31,7 +31,7 @@ class Capistrano::LocalFile < Capistrano::SCM
     # Unpack and rsync the contents into release path
     def release
       context.execute :tar, '-xvzf', "#{deploy_path}/local_file/#{fetch(:repo_url)}"
-      context.execute :rsync, "-a", "--exclude=#{fetch(:repo_url)}", "#{deploy_path}/local_file/*", release_path
+      context.execute :rsync, "-a", "--exclude=#{fetch(:repo_url)}", "#{deploy_path}/local_file", release_path
     end
 
     # TODO: SHA of archive, or variable or something to set the revision number?
